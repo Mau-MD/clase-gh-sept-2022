ff87f8c3994a924868f429e6327faf949936035a
bearlifter <tavauditore@gmail.com>
2022-09-23 13:21:31 -0700

    Create hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+
+int x, y;
+
+    cout << "Ingresa un número";
+cin >>x;
+
+    cout << "Ingresa un número";
+cin >>y;
+
+cout << x+y;
+
+
+}
